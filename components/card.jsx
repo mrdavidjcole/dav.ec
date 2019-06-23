135a818145812dde125423619af104006dd91410
Dave Cole <dave@wistia.com>
2019-06-23 16:34:14 -0400

    New approach to card repsonsiveness, and make 3d transforms look right in Firefox

--- components/card.jsx
+++ components/card.jsx
@@ -6,17 +6,18 @@ export default (props) => {
   const { getTheme } = useContext(ThemeContext)
   const { backgroundColor, foregroundColor, themeName, mildShadowColor, strongShadowColor } = getTheme();
 
+  const CARD_MARGIN_LEFT = 6;
+  const CARD_MARGIN_RIGHT = 6;
+
   return (
     <>
-      <div className="button-flex-wrapper">
-        <div className="button-responsive-padding">
-          <div className="button-responsive-wrapper">
-            <button>
-              {props.children}
-            </button>
+      <button>
+        <div className="button_content_flex_wrapper">
+          <div className="button_content">
+            {props.children}
           </div>
         </div>
-      </div>
+      </button>
       <style jsx>{`
         button {
           background-color: ${backgroundColor};
@@ -25,6 +26,7 @@ export default (props) => {
           box-shadow: 0px 6px 12px ${mildShadowColor};
           color: ${foregroundColor};
           height: 100%;
+          margin: 6px;
           position: relative;
           transition: box-shadow 200ms ease, transform 200ms ease, background-color ${THEME_TRANSITION_DURATION}ms ease-out, color ${THEME_TRANSITION_DURATION}ms ease-out;
           width: 100%;
@@ -32,62 +34,82 @@ export default (props) => {
           z-index: 0;
         }
 
-        button:hover {
+        button:hover, button:focus {
           box-shadow: 0px 20px 40px ${strongShadowColor};
-          transform: translateZ(200px);
+          transform: translateZ(10px);
           z-index: 1;
         }
 
-        .button-flex-wrapper {
-          padding: 6px;
+        button::after {
+          /* this is a hack to make the button be a responsive square. see
+          https://spin.atomicobject.com/2015/07/14/css-responsive-square/ */
+          content: '';
+          display: block;
+          padding-bottom: 100%;
         }
 
+        .button_content_flex_wrapper {
+          position: absolute;
+          left: 0;
+          top: 0;
+          width: 100%;
+          height: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        button_content {
+          flex: 0 0 auto;
+        }
+
+        /* About all these calculations...
+
+        The percentages below are simple to understand. They're 100 / (number of
+        cards to per row). The trouble is, we have margin around each card,
+        which isn't accounted for in that percentage, so 3 cards at 33.33% width
+        each can't actually fit on a row. The margin pushes off the end of it.
+        An easy way to avoid this problem would be to instead put each button in
+        a wrapper with some padding, but that breaks the perspective for
+        translateZ in Firefox. The 'perspective' property must be on the
+        _direct_ parent element of the transforming element.
+
+        tl;dr: This supports code elsewhere that makes 3d transforms look nice
+        in Firefox */
+
         @media screen and (min-width:1201px) {
-          .button-flex-wrapper {
-            flex: 1 1 14.28%;
-            max-width: 14.28%;
+          button {
+            flex: 1 1 calc(14.28% - ${CARD_MARGIN_LEFT}px - ${CARD_MARGIN_RIGHT}px);
+            max-width: calc(14.28% - ${CARD_MARGIN_LEFT}px - ${CARD_MARGIN_RIGHT}px);
           }
         }
 
         @media screen and (max-width:1200px) {
-          .button-flex-wrapper {
-            flex: 1 1 20%;
-            max-width: 20%;
+          button {
+            flex: 1 1 calc(20% - ${CARD_MARGIN_LEFT}px - ${CARD_MARGIN_RIGHT}px);
+            max-width: calc(20% - ${CARD_MARGIN_LEFT}px - ${CARD_MARGIN_RIGHT}px);
           }
         }
 
         @media screen and (max-width:900px) {
-          .button-flex-wrapper {
-            flex: 1 1 33.33%;
-            max-width: 33.33%;
+          button {
+            flex: 1 1 calc(33.33% - ${CARD_MARGIN_LEFT}px - ${CARD_MARGIN_RIGHT}px);
+            max-width: 33.calc(33% - ${CARD_MARGIN_LEFT}px - ${CARD_MARGIN_RIGHT}px);
           }
         }
 
         @media screen and (max-width:600px) {
-          .button-flex-wrapper {
-            flex: 1 1 50%;
-            max-width: 50%;
+          button {
+            flex: 1 1 calc(50% - ${CARD_MARGIN_LEFT}px - ${CARD_MARGIN_RIGHT}px);
+            max-width: calc(50% - ${CARD_MARGIN_LEFT}px - ${CARD_MARGIN_RIGHT}px);
           }
         }
 
         @media screen and (max-width:400px) {
-          .button-flex-wrapper {
-            flex: 1 1 100%;
-            max-width: 100%;
+          button {
+            flex: 1 1 calc(100% - ${CARD_MARGIN_LEFT}px - ${CARD_MARGIN_RIGHT}px);
+            max-width: 1calc(00% - ${CARD_MARGIN_LEFT}px - ${CARD_MARGIN_RIGHT}px);
           }
-        }
-
-        .button-responsive-padding {
-          padding: 100% 0 0 0;
-          position: relative;
-        }
-
-        .button-responsive-wrapper {
-          height: 100%;
-          left: 0;
-          position: absolute;
-          top: 0;
-          width: 100%;
         }
       `}</style>
     </>
